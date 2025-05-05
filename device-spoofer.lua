390dca75e0dfee2c625ce376f6bfef61b3944ad3
v-oidd <62215220+v-oidd@users.noreply.github.com>
2025-05-05 22:16:58 +0100

    Create device-spoofer.lua

---
+++ device-spoofer.lua
@@ -0,0 +1,26 @@
+targetPlatform = targetPlatform or "Windows"
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local platforms = {
+    Unknown = 0,
+    Windows = 1,
+    Linux = 2,
+    Mobile = 3,
+    Console = 4,
+    VR = 5,
+    MacOS = 6
+}
+
+local targetPlatformID = platforms[targetPlatform]
+if not targetPlatformID then return end
+
+while Players.LocalPlayer == nil do
+	Players:GetPropertyChangedSignal('LocalPlayer'):wait()
+end
+
+if Players.LocalPlayer.Character then return end
+
+Players.LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("Check").Disabled = true
+ReplicatedStorage:WaitForChild("DataTransfer").please:FireServer(targetPlatformID)
